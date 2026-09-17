1b70e75e19b2dd9056eea38aefd45f7957633e7a
Ajinkya Borade <ajinkyax@pm.me>
2026-09-17 19:42:03 +0530

    feat: UART2 Transmit a Character

--- 2_GPIO_Output/Src/main.c
+++ 2_GPIO_Output/Src/main.c
@@ -1,9 +1,10 @@
 #include <stm32f4xx.h>
 #include <stdint.h>
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+static void uart_set_baudrate(USART_TypeDef* USARTx, uint32_t PeriphClk, uint32_t BaudRate);
+static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate);
+void uart2_write(int ch);
+void uart2_tx_init();
 
 static void delay_cycles(volatile uint32_t count)
 {
@@ -11,6 +12,10 @@ static void delay_cycles(volatile uint32_t count)
         __asm("nop");
     }
 }
+
+#define SYS_FREQ		 16000000
+#define APB1_CLK		 SYS_FREQ
+#define UART_BAUDERATE 115200
 
 
 /**
@@ -27,7 +32,7 @@ static void delay_cycles(volatile uint32_t count)
  */
 #define GPIOAEN 	(1U << 0)
 #define GPIOCEN 	(1U << 2)
-#define USART2EN	(1U << 17)
+#define UART2EN	(1U << 17)
 
 #define PIN5	 	(1U<<5)
 #define PIN13	 	(1U<<13)
@@ -38,25 +43,64 @@ static void delay_cycles(volatile uint32_t count)
 
 int main(void)
 {
-	// Enable clock access
-	RCC->AHB1ENR |= GPIOAEN;
-	RCC->AHB1ENR |= GPIOCEN;
+	uart2_tx_init();
 
-	/* 2. Configure PA5 as general-purpose output (Mode 01b) */
-	GPIOA->MODER &= ~(3U << (5 * 2)); /* Shifts 0b 11 to bits 11:10 */
-	GPIOA->MODER |=  (1U << (5 * 2)); /* Set bit 10 to 1 */
-
-	/* Set PC13 as Input pin */
-	GPIOC->MODER &= ~(3U << (13 * 2)); // clears Bit 27:26 to 0 which is Input Mode
-
-    /* Loop forever */
 	while(1)
 	{
-		if (GPIOC->IDR & USER_BUTTON){
-			/// 8.4.7 GPIOx_BSRR Bit set and reset register
-			GPIOA->BSRR = LED_PIN; // BS5 Set High
-		} else {
-			GPIOA->BSRR = (LED_PIN << 16);// BR5 (1U<<21) Reset
-		}
+		uart2_write('A');
 	}
 }
+
+void uart2_tx_init()
+{
+	/**** Configure UART GPIO Pin ****/
+	RCC->AHB1ENR |= GPIOAEN;
+
+	// GPIO Pin 2 --> MODER2  sets to [1:0] alternate function mode
+	GPIOA->MODER &= ~(1U<<4); // set bit 4 to 0
+	GPIOA->MODER |= (1U<<5); // set bit 5 to 1 as per AF
+
+	// Set the AF mode to AF7 Type [0 1 1 1]
+	// negated 15U (1111 0000 1111 1111)
+	// 7U on the 8th Bit position (0111)
+	GPIOA->AFR[0] = (GPIOA->AFR[0] & ~(15U << 8)) | (7U << 8);
+
+
+	/**** Configure UART module ****/
+	// enable clock access to USART2
+	RCC->APB1ENR |= UART2EN;
+	uart_set_baudrate(USART2, APB1_CLK, UART_BAUDERATE);
+
+	// Set Transfer Direction & enable UART module
+	USART2->CR1 |= USART_CR1_TE;
+	USART2->CR1 |= USART_CR1_UE;
+}
+
+void uart2_write(int ch)
+{
+	// Wait for transmit data register is empty
+	while (!(USART2->SR & USART_SR_TXE)) {}
+
+	// Write to transmit data register
+	USART2->DR = (ch & 0xFF);
+	while (!(USART2->SR & USART_SR_TC)) {}
+}
+
+static void uart_set_baudrate(USART_TypeDef* USARTx, uint32_t PeriphClk, uint32_t BaudRate)
+{
+	USARTx->BRR = compute_uart_bd(PeriphClk, BaudRate);
+}
+
+static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate)
+{
+	return (PeriphClk + (BaudRate/2U)) / BaudRate;
+}
+
+
+
+
+
+
+
+
+
